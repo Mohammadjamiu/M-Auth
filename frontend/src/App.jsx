43be96805251206c35e35d093b888bd1b961cac0
Mohammad Jamiu <balogunmohammedjamiu@gmail.com>
2024-11-07 23:46:20 +0100

    created navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,11 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-
+import Navbar from "./components/Navbar";
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-200 firefox:bg-opacity-90">
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
-          <span className="text-2xl text-gray-900 font-semibold">Logo</span>
-          <div className="flex space-x-4 text-gray-900">
-            <a href="#">Dashboard</a>
-            <a href="#">About</a>
-            <a href="#">Projects</a>
-            <a href="#">Contact</a>
+          <Link to="/">
+            <span className="text-2xl text-gray-900 font-semibold">M-Auth</span>
+          </Link>
+
+          <div className="flex items-center space-x-4 text-gray-900">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/sign-in">Sign In</Link>
+            <Link to="/sign-up">Sign Up</Link>
+
+            {/* Profile Image */}
+            {/* <div className="ml-4">
+              <img
+                src="path-to-your-image.jpg" // Replace with the actual path to your image
+                alt="Profile"
+                className="w-10 h-10 rounded-full border-2 border-white/30 backdrop-blur-md backdrop-brightness-125"
+              />
+            </div> */}
           </div>
         </div>
       </div>
