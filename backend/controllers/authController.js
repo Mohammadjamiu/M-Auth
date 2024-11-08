6b78909f707e8043be18a47c374a58c03e6fc77e
Mohammad Jamiu <balogunmohammedjamiu@gmail.com>
2024-11-08 01:18:52 +0100

    create signup api route

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,3 +1,15 @@
-export const signup = (req, res) => {
-  console.log(req.body);
+import UserModel from "../models/UserModel.js";
+import bcryptjs from "bcryptjs";
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new UserModel({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json({
+      message: "User created successfully!",
+    });
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 };
