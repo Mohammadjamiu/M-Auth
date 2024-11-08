556e980445038e2bac90cfc82b8ae50e340dfad3
Mohammad Jamiu <balogunmohammedjamiu@gmail.com>
2024-11-08 01:04:04 +0100

    added models and routes and controllers and express middlewares

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import mongoose from "mongoose";
+import userRoutes from "./routes/userRoute.js";
+import authRoutes from "./routes/authRoute.js";
 dotenv.config();
 
 //! Initialize Express App and PORT
 const app = express();
 const PORT = process.env.PORT || 4500;
+
+//! Express Middlewares
+app.use(express.json()); // To parse the JSON data from Req.body
 
 //! Connection to the MONGO DB
 mongoose
@@ -22,3 +27,7 @@ mongoose
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`.blue.underline);
 });
+
+//! Api Route
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
